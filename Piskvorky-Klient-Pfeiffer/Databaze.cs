143bc6d96b3d6c4df2f4de079d37cfa2bd5dd9e0
Lukasinho <lukasinho.9@gmail.com>
2013-07-14 20:50:11 +0200

    Síť, Databáze
    Přispusobení souborů pro WPF

---
+++ Piskvorky-Klient-Pfeiffer/Databaze.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Piskvorky_Klient_Pfeiffer
+{
+    public class Databaze
+    {
+        private string ip;
+        SqlConnection datovePripojeni = new SqlConnection();
+        public Databaze(string ipadresa)
+        {
+            ip = ipadresa;
+
+        }
+
+
+        public void Pripojit()
+        {
+            try
+            {
+                SqlConnectionStringBuilder konfigurace = new SqlConnectionStringBuilder();
+                konfigurace.DataSource = ".\\SQLExpress";
+                konfigurace.InitialCatalog = "Piskvorky";
+                konfigurace.IntegratedSecurity = true;
+                datovePripojeni.ConnectionString = konfigurace.ConnectionString;
+                datovePripojeni.Open();
+            }
+            catch (SqlException e)
+            {
+                MessageBox.Show("Chyba při vstupu do DB: {0}", e.Message);
+            }
+        }
+
+        public void PridatHrace(string jmenoHrace)
+        {
+
+            try
+            {
+                
+                SqlCommand datovyPrikaz = new SqlCommand();
+                datovyPrikaz.Connection = datovePripojeni;
+                datovyPrikaz.CommandType = CommandType.Text;
+                datovyPrikaz.CommandText =
+                    "Insert Into Hrac(pocet_bodu,jmeno) output inserted.id values (0,@JmenoHrace)";
+
+                SqlParameter param = new SqlParameter("@JmenoHrace", SqlDbType.VarChar, 20);
+                param.Value = jmenoHrace;
+                datovyPrikaz.Parameters.Add(param);
+
+              //  datovyPrikaz.ExecuteNonQuery();
+                int id = (int) datovyPrikaz.ExecuteScalar();    //RIKA ID VLOZENEHO HRACE
+            }
+            catch (SqlException e)
+            {
+                MessageBox.Show("Chyba vložení do DB: "+e.Message);
+           
+            }
+            finally
+            {
+                datovePripojeni.Close();
+            }
+            
+           
+
+        }
+        public void Odpojit()
+        {
+            datovePripojeni.Close();
+        }
+
+
+        internal void PridatPravidlo(string p)
+        {
+            try
+            {
+
+                SqlCommand datovyPrikaz = new SqlCommand();
+                datovyPrikaz.Connection = datovePripojeni;
+                datovyPrikaz.CommandType = CommandType.Text;
+                datovyPrikaz.CommandText =
+                    "Insert Into Pravidlo(hrac_id,pravidlo) values (10,@p)";
+
+                SqlParameter param = new SqlParameter("@p", SqlDbType.VarChar, 60);
+                param.Value = p;
+                datovyPrikaz.Parameters.Add(param);
+
+                datovyPrikaz.ExecuteNonQuery();
+
+            }
+            catch (SqlException e)
+            {
+                MessageBox.Show("Chyba vložení do DB: {0}", e.Message);
+
+            }
+            finally
+            {
+                datovePripojeni.Close();
+            }
+        }
+    }
+}
---
+++ Piskvorky-Klient-Pfeiffer/Sit.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Piskvorky_Klient_Pfeiffer
+{
+    class Sit
+    {
+        string ipadresa;
+        int cisloPortu;
+        public Sit(string ip, int port)
+        {
+            ipadresa = ip;
+            cisloPortu = port;
+        }
+
+        public void Poslouchej()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Parse(ipadresa), cisloPortu);
+            TcpClient client = null;
+            try
+            {
+                while (true)
+                {
+
+                    listener.Start();
+                    client = listener.AcceptTcpClient();
+                    NetworkStream clientStream = client.GetStream();
+                    StreamReader reader = new StreamReader(clientStream);
+                    string obsahZpravy = reader.ReadToEnd();
+                    MessageBox.Show("Klient prijal: " + obsahZpravy);
+
+
+                    //ROZKODOVAT SI TYP ZPRAVY
+                    //Prichozi: Hrajes , Kolecko, Krizek
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Nastala chyba v poslechu " + e);
+            }
+            finally
+            {
+                if (client != null)
+                {
+                    client.Close();
+                }
+                listener.Stop();
+            }
+
+        }
+
+        public void Odesli(string data)
+        {
+            TcpClient client = null;
+            try
+            {
+                client = new TcpClient(ipadresa, 5000);      //adresa serveru
+                Stream strm = client.GetStream();
+                StreamWriter writer = new StreamWriter(strm);
+                writer.Write(data);
+                writer.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Nastala chyba v odesilani " + e);
+
+            }
+            finally
+            {
+                if (client != null)
+                    client.Close();
+
+            }
+        }
+
+
+    }
+}
